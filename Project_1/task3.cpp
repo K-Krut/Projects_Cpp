9dcadca80936be6cad11cf85408cd64f56deb11e
kekekek <90685950+K-Krut@users.noreply.github.com>
2022-04-08 17:16:14 +0300

    Update task3.cpp

--- Project_1/task3.cpp
+++ Project_1/task3.cpp
@@ -35,3 +35,68 @@ int main(){
 Ця функція може використовувати як і раніше виділену ділянку пам'яті,
 так і нову
 */
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Nameval {
+  char *name;
+  int value;
+} Nameval;
+
+struct Array {
+  int nval;
+  int max;
+  Nameval *items;
+};
+
+enum { NVINIT = 1, NVGROW = 2 };
+
+  int add_item(struct Array *array, Nameval newname){  
+    Nameval *temp;
+     if (array->items == NULL) {
+      array->items = (Nameval *)malloc(NVINIT * sizeof(Nameval));
+      if(array->items == NULL)
+        return -1;
+      array->max = NVINIT;
+      array->nval = 0;
+    }
+    else if (array->nval >= array->max) {
+      temp = (Nameval *) realloc(array->items, 
+        (NVGROW * array->max) * sizeof(Nameval));
+      if (temp == NULL)
+        return -1;
+      array->max *= NVGROW;
+      array->items = temp;
+    }
+    array->items[array->nval] = newname;
+    return ++array->nval;
+  }
+
+  int del_item(struct Array *array, char *name) {
+    for (int i = 0; i<array->nval; ++i){
+      if (strcmp(array->items[i].name, name) == 0){
+        memmove(array->items + i, array->items + i + 1, (array->nval - (i + 1)) * sizeof(Nameval));
+        --array->nval;
+        return 1;
+      }
+    }
+    return 0;  
+  }
+
+int main() {
+  struct Array array = { 0, 0, NULL }; //Initializing fields of a structure
+  add_item(&array, (struct Nameval) {.name = "Andy", .value = 12});
+  add_item(&array, (struct Nameval){.name = "Billy", .value = 16});
+  add_item(&array, (struct Nameval){.name = "Tom", .value= 25});
+  add_item(&array, (struct Nameval){.name = "Alice", .value= 25});
+
+  for (int i = 0; i < array.nval; ++i){
+    printf("%s %d\n", array.items[i].name, array.items[i].value);
+  }
+  return 0;
+}
